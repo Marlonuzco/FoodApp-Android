884a75953ca4570234446b063c50267618374a97
Marlon Uzcátegui <mariodaviduzcategui@gmail.com>
2023-09-15 01:29:58 -0400

    changes in axios

--- axios.js
+++ axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: 'https://6e21-200-59-186-100.ngrok-free.app',
+  baseURL: '',
 });
 
 export const serverUrl = axiosInstance.getUri();
